9348fa3cb98b655ba7a3cd1b8e2cb54ab88faf93
vedkatoch <honeykatoch388@gmail.com>
2026-02-26 15:03:21 +0530

    Add SQL queries for college data analysis

--- MEAGA DATA.sql
+++ MEAGA DATA.sql
@@ -25,9 +25,44 @@ WHERE (
         OR TRIM(College_TPO_Contact_Number) = ''    #TRIM() removes spaces from left and right.
         OR UPPER(College_TPO_Contact_Number) = 'NA' #UPPER() changes text to CAPITAL letters.
       );
+select * from  MEGA_COLLAGE_DATA  where State='Ladakh' and Student_Strength>400 and Region_North_South_East_West_Central='NORTH';
+# TELL ME THE COLLAGE COUT BY STATE WISE##########
+select COUNT(College_Name) AS COLLAGE_COUNT,state FROM MEGA_COLLAGE_DATA group by state order by COLLAGE_COUNT desc ;
+#######Tell me total studentstrught by region ########
+select Region_North_South_East_West_Central,COUNT(Student_Strength)  FROM MEGA_COLLAGE_DATA group by Region_North_South_East_West_Central  ;
+#########################################################
+#Find top 5 cities having the highest number of colleges.
+select count(College_Name) as collage_count,City  FROM MEGA_COLLAGE_DATA group by city order by collage_count desc limit 5;
+#Find all colleges where landline number is missing.
+select *from  MEGA_COLLAGE_DATA where landline_no=0 ;
+#Count private colleges in each state.
+select state,count(college_name) as privite_collage from MEGA_COLLAGE_DATA where college_type='private' group by  state order by privite_collage desc ;
+################################
+##################################### advance ######################### 
+	#Find the state having the highest total student strength.
+select state ,sum(Student_Strength) as total from  MEGA_COLLAGE_DATA group by state order by total  desc limit 1;
+  
+#Find colleges whose student strength is above average.
+select avg(Student_Strength) as average_ans from mega_collage_data;
+SELECT college_name, Student_Strength FROM MEGA_COLLAGE_DATA
+ WHERE Student_Strength > (
+    SELECT AVG(Student_Strength) 
+    FROM MEGA_COLLAGE_DATA
+);
+#Rank colleges based on student strength.
 
-      
-
+#Find duplicate college names in the dataset.
+#Find which region has the maximum MBA colleges.
+select COUNT(College_Type),Region_North_South_East_West_Central from mega_collage_data 
+where College_Type='MBA'  group by College_Type >(select max(Region_North_South_East_West_Central) from mega_collage_data);
+SELECT Region_North_South_East_West_Central, COUNT(*) AS mba_college_count
+FROM mega_collage_data
+WHERE College_Type = 'MBA'
+GROUP BY Region_North_South_East_West_Central
+ORDER BY mba_college_count DESC
+LIMIT 2;
+#############################################################
+select *from MEGA_COLLAGE_DATA ;
       
 
 
